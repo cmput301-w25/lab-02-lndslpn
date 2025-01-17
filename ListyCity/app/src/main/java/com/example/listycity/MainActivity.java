c1d1b4095ab8c97313b4f58eaf509c8cc119c778
Landys Lepine <landys@ualberta.ca>
2025-01-17 10:27:18 -0700

    completed & working

--- ListyCity/app/src/main/java/com/example/listycity/MainActivity.java
+++ ListyCity/app/src/main/java/com/example/listycity/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.listycity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
 
 import androidx.activity.EdgeToEdge;
@@ -15,9 +19,12 @@ import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
 
-    ListView cityList;
-    ArrayAdapter<String> cityAdapter;
-    ArrayList<String> dataList;
+    String city;
+    ListView cityList; //reference for list view
+    ArrayAdapter<String> cityAdapter; //reference for array adapter
+    ArrayList<String> dataList; //reference for array list
+    Button add, delete, confirm;
+    EditText cityInput;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,15 +38,55 @@ public class MainActivity extends AppCompatActivity {
             return insets;
         });
 
-        cityList = findViewById(R.id.city_list);
+        cityList = findViewById(R.id.city_list); //finding reference to ListView
+        cityInput = findViewById(R.id.input_text);
+        add = findViewById(R.id.add_button);
+        delete = (Button) findViewById(R.id.delete_button);
+        confirm = findViewById(R.id.confirm_button);
 
-        String[] cities = {"Edmonton", "Paris", "London"};
+        String[] cities = {"Edmonton", "Paris", "London"}; //declaring a string array
 
         dataList = new ArrayList<>();
-        dataList.addAll(Arrays.asList(cities));
+        dataList.addAll(Arrays.asList(cities)); //feeding city array into ListView
 
         cityAdapter = new ArrayAdapter<>(this,R.layout.content, dataList);
         cityList.setAdapter(cityAdapter);
 
+        //making confirm button and input box become visible when you click add
+        add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                confirm.setVisibility(View.VISIBLE);
+                cityInput.setVisibility(View.VISIBLE);
+            }
+        });
+
+        //pressing confirm adds city
+        confirm.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                city = cityInput.getText().toString();
+                dataList.add(city);
+                cityAdapter.notifyDataSetChanged();
+                confirm.setVisibility(View.GONE);
+                cityInput.setVisibility(View.GONE);
+            }
+        });
+
+        //deleting city when you click it
+        cityList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+            delete.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    dataList.remove(position);
+                    cityAdapter.notifyDataSetChanged();
+                }
+            });
+
+            }
+        });
     }
 }
